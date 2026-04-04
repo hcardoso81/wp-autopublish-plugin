4b24d01e98a9b3f5991c4090bd93b917319ebff8
Fede Cardoso <hernan.f.cardoso@gmail.com>
2026-04-04 19:52:18 -0300

    upload folder

--- admin/UploadPageController.php
+++ admin/UploadPageController.php
@@ -26,23 +26,50 @@ class UploadPageController
     private function handleUpload(): void
     {
         try {
-            Logger::info('Upload iniciado');
+           Logger::info('Upload carpeta iniciado');
 
-            if (!isset($_FILES['html_file'])) {
-                throw new \Exception('No file uploaded');
+            if (!isset($_FILES['files'])) {
+                throw new \Exception('No files uploaded');
             }
 
-            $html = file_get_contents($_FILES['html_file']['tmp_name']);
+            $files = $_FILES['files'];
+
+            $uploadedFiles = [];
+
+            foreach ($files['name'] as $index => $name) {
+                $tmpPath = $files['tmp_name'][$index];
+
+                if (!$tmpPath) continue;
+
+                $uploadedFiles[] = [
+                    'name' => $name,
+                    'tmp_path' => $tmpPath
+                ];
+            }
+
+            // Buscar el HTML
+            $htmlFile = null;
+
+            foreach ($uploadedFiles as $file) {
+                if (str_ends_with($file['name'], '.html')) {
+                    $htmlFile = $file;
+                    break;
+                }
+            }
+
+            if (!$htmlFile) {
+                throw new \Exception('No HTML file found in upload');
+            }
+
+            $html = file_get_contents($htmlFile['tmp_path']);
 
             $postId = $this->importer->createDraftFromHtml($html);
-
             Logger::info('Post creado', ['postId' => $postId]);
 
             echo '<div class="notice notice-success"><p>Post creado (ID: ' . $postId . ')</p></div>';
-
         } catch (\Exception $e) {
-            Logger::error('Error en upload', [
-                'message' => $e->getMessage()
+            Logger::error('Error upload carpeta', [
+                'error' => $e->getMessage()
             ]);
 
             echo '<div class="notice notice-error"><p>Error: ' . $e->getMessage() . '</p></div>';
--- views/upload-page.php
+++ views/upload-page.php
@@ -2,7 +2,7 @@
     <h1>AutoPublish</h1>
 
     <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="html_file" accept=".html" required />
+        <input type="file" name="files[]" webkitdirectory directory multiple />
         <br><br>
 
         <button class="button button-primary">
